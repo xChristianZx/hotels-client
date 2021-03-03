de3ad06d1d810a33f8955ab17934e36e7b39c3ba
Christian Zenaty <cazenaty@yahoo.com>
2021-03-03 11:33:18 -0800

    Add initial ShowHotel page layout

---
+++ pages/hotels/[hotelId].js
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/router';
+import { trimHotelName } from '../../utils/helper';
+import RoomTypeItem from '../../components/hotels/RoomTypeItem';
+
+const ShowHotel = ({ hotel }) => {
+  function renderRoomTypesList(list) {
+    return (
+      <ul className="flex flex-col flex-grow p-2">
+        {list.map(room => (
+          <RoomTypeItem key={room.roomTypeId} room={room} />
+        ))}
+      </ul>
+    );
+  }
+  return hotel ? renderRoomTypesList(hotel.roomTypes) : <p>Loading...</p>;
+};
+
+export async function getServerSideProps({ params }) {
+  const res = await fetch(`http://localhost:4000/${params.hotelId}`);
+  const data = await res.json();
+  return { props: { hotel: data } };
+}
+
+export default ShowHotel;
