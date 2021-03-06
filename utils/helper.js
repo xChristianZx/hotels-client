6de2546cd5c50fabb62e2a64216ecba921af5694
Christian Zenaty <cazenaty@yahoo.com>
2021-03-05 16:48:52 -0800

    Add MIN_START_DATE for date inputs

--- utils/helper.js
+++ utils/helper.js
@@ -7,3 +7,9 @@ export function trimHotelName(str) {
   const regex = /\[SANDBOX]/gi;
   return str.replace(regex, '');
 }
+
+/**
+ * For Date type inputs to set the minimum start date to the local
+ * day in the YYYY-MM-DD format.
+ */
+export const MIN_START_DATE = new Date().toLocaleDateString('en-CA');
