24b66d8ad26ba8f9f5ba230694dd4138374af5da
Christian Zenaty <cazenaty@yahoo.com>
2021-03-17 22:58:05 -0700

    Add diffInDays helper function

--- utils/helper.js
+++ utils/helper.js
@@ -1,7 +1,9 @@
+import { DateTime } from 'luxon';
+
 /**
  * Removes "[SANDBOX]" from hotel name
  * from Impala sandbox api
- * @param {*} str
+ * @param {string} str
  */
 export function trimHotelName(str) {
   const regex = /\[SANDBOX]/gi;
@@ -13,3 +15,21 @@ export function trimHotelName(str) {
  * day in the YYYY-MM-DD format.
  */
 export const MIN_START_DATE = new Date().toLocaleDateString('en-CA');
+
+/**
+ * Returns the number of days between two dates.
+ *
+ * @param {string} startDate - string in ISO 8601 format - ex. 'yyyy-mm-dd'
+ * @param {string} endDate - string in ISO 8601 format - ex. 'yyyy-mm-dd'
+ * @returns {number} - Number of days
+ */
+export const diffInDays = (startDate, endDate) => {
+  const start = DateTime.fromISO(startDate);
+  const end = DateTime.fromISO(endDate);
+
+  const diffDay = end.diff(start, 'days');
+
+  const pDiff = diffDay.toObject();
+
+  return pDiff.days;
+};
