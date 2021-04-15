f69495ce96ba28b61f6e6d2d299d2531560bc67c
Christian Zenaty <cazenaty@yahoo.com>
2021-04-15 14:11:47 -0700

    Update buildShowRouteUrl to include destination

--- components/hotels/HotelItem.js
+++ components/hotels/HotelItem.js
@@ -1,20 +1,23 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { stringify } from 'qs';
 import Carousel from '../carousel/Carousel';
 import { trimHotelName } from '../../utils/helper';
 
 export default function HotelItem({ hotel, destination, startDate, endDate }) {
-  function buildShowRouteUrl(hotelId, startDate, endDate) {
-    let baseUrl = `/hotels/${hotelId}?`;
-    if (destination && startDate && endDate) {
-      const qStr = `country%5Beq%5D=${destination}&`;
-      baseUrl = baseUrl + qStr;
+  function buildShowRouteUrl(hotelId, destination, startDate, endDate) {
+    const baseUrl = `/hotels/${hotelId}`;
+    let paramsObj = {};
+    if (destination) {
+      paramsObj = { ...paramsObj, country: { eq: destination } };
     }
     if (startDate && endDate) {
-      const qStr = `start=${startDate}&end=${endDate}`;
-      baseUrl = baseUrl + qStr;
+      paramsObj = { ...paramsObj, start: startDate, end: endDate };
     }
-    return baseUrl;
+
+    return (
+      baseUrl + stringify(paramsObj, { encode: false, addQueryPrefix: true })
+    );
   }
   return (
     <li className="flex flex-col lg:flex-row justify-center items-center h-screen lg:h-auto w-full mb-2 p-4 bg-white">
@@ -29,7 +32,14 @@ export default function HotelItem({ hotel, destination, startDate, endDate }) {
           {hotel.address.city}, {hotel.address.country}
         </p>
         <p className="p-4">{hotel.starRating} Stars</p>
-        <Link href={buildShowRouteUrl(hotel.hotelId, startDate, endDate)}>
+        <Link
+          href={buildShowRouteUrl(
+            hotel.hotelId,
+            destination,
+            startDate,
+            endDate
+          )}
+        >
           <button className="flex items-center justify-center px-8 py-3 border text-base text-white font-medium bg-gray-900">
             Book Now
           </button>
