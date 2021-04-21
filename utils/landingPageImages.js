a186ca548c50de5c9d8af621222445e61d97938c
Christian Zenaty <cazenaty@yahoo.com>
2021-04-21 16:08:50 -0700

    Add landingPageImages array

---
+++ utils/landingPageImages.js
@@ -0,0 +1,35 @@
+export const heroImages = [
+  {
+    address: { city: 'Santorini', countryName: 'Greece' },
+    hotelId: 'd1504c14-2de8-4653-b483-ae0fea8d79b3',
+    name: 'La Perla Villas & Suites',
+    url: 'https://cdn.impala.travel/properties/ckhoon87h000d3b6670qxox7o.jpg',
+  },
+  {
+    address: {
+      city: 'Costa Adeje',
+      countryName: 'Spain',
+    },
+    hotelId: '644702f5-b6a4-4312-9ab6-7380d0ffa157',
+    name: 'Iberostar Grand El Mirador ',
+    url: 'https://cdn.impala.travel/properties/ckk3xzg2c000f3h61xa1zalk0.jpg',
+  },
+  {
+    address: {
+      city: 'Miami Beach',
+      countryName: 'United States of America',
+    },
+    hotelId: 'cefd5621-737f-4929-9967-b74c4d4784cc',
+    name: 'The Setai, Miami Beach',
+    url: 'https://cdn.impala.travel/properties/ckc0f9woi005f30z4hwe9baba.jpg',
+  },
+  {
+    address: {
+      city: 'Hammam Sousse',
+      countryName: 'Tunisia',
+    },
+    hotelId: '219e3fa3-0410-4ebd-a5d8-f2788ee45780',
+    name: 'Iberostar Selection Kantaoui Bay',
+    url: 'https://cdn.impala.travel/properties/ckl10mkf1000f3h6bciukbe5z.webp',
+  },
+];
