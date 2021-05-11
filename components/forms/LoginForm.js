a6354824ab5f2a66d1b1083332b3f0df788b3666
Christian Zenaty <cazenaty@yahoo.com>
2021-05-11 14:14:42 -0700

    Initial login form UI layout

---
+++ components/forms/LoginForm.js
@@ -0,0 +1,74 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Valid email is required')
+    .required('Valid email is required'),
+  password: yup.string().required('Password is required'),
+});
+
+export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(loginSchema) });
+
+  const onSubmit = data => console.log('SUBMITTED', data);
+
+  return (
+    <div className="w-full p-4 lg:w-1/3 sm:max-w-md bg-gray-50 bg-opacity-95 shadow-2xl z-50">
+      <div className="py-8 text-center">
+        <h1 className="text-3xl tracking-wide">Login</h1>
+      </div>
+      <form
+        className="flex flex-col w-full p-4 space-y-4 font-normal"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label
+          htmlFor="email"
+          className="flex flex-col justify-center items-start px-4"
+        >
+          <span className="pl-2">Email</span>
+          <input
+            id="email"
+            className="w-full py-2 border-0 border-b border-gray-300 focus:outline-none focus:border-gray-900 focus:ring-0 bg-transparent"
+            type="text"
+            {...register('email')}
+          />
+          <p className="h-9 pt-2 text-sm text-red-600">
+            {errors.email && <span role="alert">{errors.email?.message}</span>}
+          </p>
+        </label>
+
+        <label
+          htmlFor="password"
+          className="flex flex-col justify-center items-start px-4"
+        >
+          <span className="pl-2">Password</span>
+          <input
+            id="password"
+            className="w-full py-2 border-0 border-b border-gray-300 focus:outline-none focus:border-gray-900 focus:ring-0 bg-transparent"
+            type="password"
+            {...register('password')}
+          />
+          <p role="alert" className="h-9 pt-2 text-sm text-red-600">
+            {errors.password && (
+              <span role="alert">{errors.password?.message}</span>
+            )}
+          </p>
+        </label>
+        <div className="w-full px-4">
+          <input
+            className="flex items-stretch justify-center px-8 py-2 w-full border-b border-gray-900 text-base text-white font-medium cursor-pointer uppercase tracking-wider bg-gray-900 hover:bg-gray-800"
+            type="submit"
+            value="Login"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,3 +1,26 @@
+import Link from 'next/link';
+import LoginForm from '../../components/forms/LoginForm';
+import { heroImages } from '../../utils/landingPageImages';
+
 export default function Login() {
-  return <div>LOGIN PAGE TO BE BUILT!</div>;
+  const currentHero = heroImages[2];
+  return (
+    <div className="relative sm:px-4 pt-16 bg-gray-200 flex flex-col flex-grow justify-start items-center">
+      <img
+        className="absolute inset-0 z-0 w-full h-full object-cover object-bottom filter hue-rotate-15"
+        src={currentHero.url}
+      />
+      <div className="absolute bottom-0 right-0 p-3 text-gray-300 hover:text-white bg-gray-400 bg-opacity-20 hover:bg-opacity-40 italic text-xs xl:text-lg">
+        <Link href={`/hotels/${currentHero.hotelId}`}>
+          <a className="">
+            <p className="font-light">{currentHero.name}</p>
+            <p className="font-thin">
+              {currentHero.address.city}, {currentHero.address.countryName}
+            </p>
+          </a>
+        </Link>
+      </div>
+      <LoginForm />
+    </div>
+  );
 }
