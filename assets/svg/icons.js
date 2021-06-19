8354cb51a42f89284b5ed2974e68882f957cd682
Christian Zenaty <cazenaty@yahoo.com>
2021-06-18 18:33:00 -0700

    Add Chevron and refactor icons

---
+++ assets/svg/icons.js
@@ -0,0 +1,75 @@
+export function CalendarSVG(props) {
+  return (
+    <svg
+      className={props.className || 'w-6 h-6'}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={props.strokeWidth || 1}
+        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+      />
+    </svg>
+  );
+}
+
+export function ChevronDownSVG(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={props.className || 'h-4 w-4'}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={props.StrokeWidth || 1}
+        d="M19 9l-7 7-7-7"
+      />
+    </svg>
+  );
+}
+
+export function LogoutSVG(props) {
+  return (
+    <svg
+      className={props.className || 'w-4 h-4'}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={props.strokeWidth || 1}
+        d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+      />
+    </svg>
+  );
+}
+
+export function UserSVG(props) {
+  return (
+    <svg
+      className={props.className || 'w-6 h-6'}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={props.strokeWidth || 1}
+        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+      />
+    </svg>
+  );
+}
